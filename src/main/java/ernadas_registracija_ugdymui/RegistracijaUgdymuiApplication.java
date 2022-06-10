d7365357d6cf01d1b572ea14626ec407feb446db
q777w <q777w@protonmail.com>
2022-06-10 09:36:31 +0300

    pradnis kodas

---
+++ src/main/java/ernadas_registracija_ugdymui/RegistracijaUgdymuiApplication.java
@@ -0,0 +1,13 @@
+package ernadas_registracija_ugdymui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistracijaUgdymuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistracijaUgdymuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ernadas_registracija_ugdymui/RegistracijaUgdymuiApplicationTests.java
@@ -0,0 +1,13 @@
+package ernadas_registracija_ugdymui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistracijaUgdymuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
